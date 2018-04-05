471002c9c677ff33a4a8e6680ec985732cf806d6
Steve Hogan <stevenjameshogan@gmail.com>
2018-04-05 10:59:58 -0500

    Server route working, pool connected to db, GET route working on server side

--- server/server.js
+++ server/server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = 3086;
 const bodyParser = require('body-parser');
+const recordsRouter = require('./routes/records.router.js');
 
 
 // Configure body-parser
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json()); // This line is required for Angular
+
+
+app.use('/records', recordsRouter);
 
 app.use(express.static('server/public'))
 
